a4bb6d413ae7ad05f176325a6e90fb6f64526738
hrs2203 <hrishabh2203@gmail.com>
2021-01-23 14:41:37 +0530

    upd2

--- sample1.py
+++ sample1.py
@@ -2,28 +2,21 @@ import streamlit as st
 import os
 
 ## ======= static file locaton ============
-
-FILE_DIRECTORY = os.path.join(".","uploadedFiles")
-
 ## ========== server code =================
 
-st.title("Image Caption System")
+@st.cache
+def homepage():
+	st.title("Image Caption System")
 
-uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg'])
+	uploaded_file = st.file_uploader("Upload Files (.png and .jpg only)",type=['png','jpg'])
 
-if uploaded_file is not None:
 	# use our model here and show its caption.
-
-
-
-	file_details = {
-		"FileName":uploaded_file.name,
-		"FileType":uploaded_file.type,
-		"FileSize":uploaded_file.size,
-		"FileDescription": "Lorem Ipsem for now."
-	}
-	st.image(uploaded_file, height=250, width=250)
-	st.write(file_details)
-
-
+	if uploaded_file is not None:
+		file_details = {
+			"FileName":uploaded_file.name,
+			"FileType":uploaded_file.type,
+			"FileDescription": "Lorem Ipsem for now."
+		}
+		st.image(uploaded_file, use_column_width=True)
+		st.write(file_details)
 
